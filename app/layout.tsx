a96d3c459f0a65840d35de90706979547459a775
singlesyntax <thefaruksardar@gmail.com>
2024-02-22 22:44:18 +0530

    gsc code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="0vYFInUhgrQmSX5AUuKVL8tkMFutW8RhlPBT7JoytlE"
+        />
         <meta name="google-adsense-account" content="ca-pub-1436861775123746" />
       </head>
       <body className={`${inter.className} bg-slate-50`}>
